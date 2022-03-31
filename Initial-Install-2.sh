3452953aec67779fd2d9dfa3e258455ac7b2ceba
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-03-31 07:47:26 -0500

    Update install config

---
+++ Initial-Install-2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install chezmoi
+echo "Installing chezmoi..."
+sh -c "$(curl -fsLS chezmoi.io/get)"
+bin/chezmoi init --apply git@github.com:dakotaruelle/dotfiles.git
+
+echo "Checking for HomeBrew installation..."
+if [ -d /home/linuxbrew ]; then
+    echo "HomeBrew is already installed"
+else
+    echo "Installing HomeBrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+    echo "Adding HomeBrew to path..."
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+brew tap jandedobbeleer/oh-my-posh
+brew install oh-my-posh
