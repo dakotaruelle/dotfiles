3cd34ed0f8544276887f12147f2e4268908af063
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2022-07-20 15:39:50 -0500

    Update install script

--- devcontainer-install.sh
+++ devcontainer-install.sh
@@ -4,10 +4,17 @@ mv -f $HOME/dotfiles/* $HOME/
 
 # Install chezmoi
 sh -c "$(curl -fsLS chezmoi.io/get)"
+touch $HOME/.config/chezmoi/chezmoi.yaml
+echo "data:" >> $HOME/.config/chezmoi/chezmoi.yaml
+echo "  name: work-computer" >> $HOME/.config/chezmoi/chezmoi.yaml
 
-# Add homebrew to path
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install oh-my-posh
 brew tap jandedobbeleer/oh-my-posh
 brew install oh-my-posh
+
+# Init
+bin/chezmoi init --apply git@github.com:dakotaruelle/dotfiles.git
