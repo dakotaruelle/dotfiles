13343b7d37a36506a5ed7b251003baeecd75c175
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2023-02-13 11:35:00 -0600

    Update install scripts

--- Initial-Install-1.sh
+++ Initial-Install-1.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
-### Upgrade default packages
-echo "Upgrading default packages..."
+NVM_DIR="/home/dakota/.nvm"
+HUSH_LOGIN_PATH="/home/dakota/.hushlogin"
+
+## Upgrade default packages
+printf "\n-----  Upgrading default packages...  -----\n"
 sudo apt update
 sudo apt upgrade -y
 
-### Install Packages
-echo "Installing packages..."
-sudo apt install gcc zsh -y
+## Install Packages
+printf "\n-----  Installing new packages...  -----\n"
+sudo apt install git gcc build-essential libssl-dev curl -y
+
+### Install oh-my-zsh
+if ( which zsh )
+then
+  printf "\n-----  zsh already installed  -----\n"
+else
+  printf "\n-----  Installing oh-my-zsh...  -----\n"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 ### Install nvm and node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install --lts
+if ( test -d $NVM_DIR )
+then
+  printf "\n-----  nvm and node already installed  -----\n"
+else
+  printf "\n-----  Installing nvm and node...  -----\n"
 
-### Install global node packages
-npm install --global yarn pnpm
+  mkdir -p $NVM_DIR
+  export NVM_DIR="$NVM_DIR"
+  
+  sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  
+  export NVM_DIR="$NVM_DIR"
+  [ ?-s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  nvm install --lts
 
-### Configure zsh
-echo "Setting zsh as default shell..."
-sudo chsh -s $(which zsh)
+fi
 
 ### Silence daily prompt
-echo "Creating .hushlogin file..."
-touch ~/.hushlogin
+if ( test -f $HUSH_LOGIN_PATH )
+then
+  printf "\n-----  .hushlogin already exists  -----\n"
+else
+  printf "\n-----  Creating .hushlogin file  -----\n"
+  touch $HUSH_LOGIN_PATH
+fi
--- Initial-Install-2.sh
+++ Initial-Install-2.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-# Install chezmoi
-echo "Installing chezmoi..."
-sh -c "$(curl -fsLS chezmoi.io/get)"
-bin/chezmoi init --apply git@github.com:dakotaruelle/dotfiles.git
+CHEZMOI_PATH="/home/dakota/bin/chezmoi"
+CHEZMOI_CONFIG_PATH="/home/dakota/.config/chezmoi/chezmoi.yaml"
 
-echo "Checking for HomeBrew installation..."
-if [ -d /home/linuxbrew ]; then
-    echo "HomeBrew is already installed"
+### Install chezmoi
+if ( test -f $CHEZMOI_PATH )
+then
+  printf "\n-----  chezmoi already installed  -----\n"
 else
-    echo "Installing HomeBrew..."
+  printf "\n-----  Installing chezmoi...  -----\n"
+  sh -c "$(curl -fsLS chezmoi.io/get)"
+  touch $CHEZMOI_CONFIG_PATH
+  echo "data:" >> $CHEZMOI_CONFIG_PATH
+  echo "  name: home-computer" >> $CHEZMOI_CONFIG_PATH
+  bin/chezmoi init --apply git@github.com:dakotaruelle/dotfiles.git
+fi
+
+if [ -d /home/linuxbrew ]; then
+    printf "\n-----  HomeBrew already installed  -----\n"
+else
+    printf "\n-----  Installing HomeBrew...  -----\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     
-    echo "Adding HomeBrew to path..."
+    printf "\n-----  Adding HomeBrew to path...  -----\n"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
