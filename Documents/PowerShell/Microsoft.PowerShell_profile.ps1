555f15242c959e7beefa74b71f3a219c2f1b4d37
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2024-01-16 20:00:07 -0600

    Update powershell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 oh-my-posh init pwsh --config ~/.oh-my-posh.config.json | Invoke-Expression
 $env:CHROME_EXECUTABLE="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+New-Alias ExportStartMenuLinks Export-StartLayout -Path "C:\Layouts\GetIDorPath.xml"
