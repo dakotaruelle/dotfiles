04f2c5b91ef2e54b6230205c567e2f4081dc4088
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2025-04-17 11:05:40 -0500

    Delete unused files

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-oh-my-posh init pwsh --config ~/.oh-my-posh.config.json | Invoke-Expression
-$env:CHROME_EXECUTABLE="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
-
-Function ExportStartLayout { Export-StartLayout -Path "$HOME\GetIDorPath.xml" }
-
-New-Alias Export-StartMenuLinks ExportStartLayout
--- initial-install/Install.ps1
+++
@@ -1,3 +0,0 @@
-### Customize taskbar
-Write-Output "Updating taskbar..."
-Import-StartLayout -LayoutPath ".windows-taskbar-layout.xml" -MountPath "C:\"
